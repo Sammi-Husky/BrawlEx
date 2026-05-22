da61429ced78cbecb1b697a83adb47914d8db8b5
Sammi Husky <sammi-husky@live.com>
2026-05-22 16:22:10 -0700

    Remove reference disassembly and agent file

--- bx_fighter/disasm/bx_fighter/Section[2].asm
+++
@@ -1,6 +0,0 @@
-loc_0:
-        .4byte 0x00000000         [R_PPC_ADDR32(128, 1, "loc_0")]
-        .4byte 0x00000000         [R_PPC_ADDR32(128, 1, "loc_130")]
-        .4byte 0x00000000         [R_PPC_ADDR32(128, 1, "loc_240")]
-        .4byte 0x00000000         [R_PPC_ADDR32(128, 1, "loc_3A0")]
-        .4byte 0x00000000
